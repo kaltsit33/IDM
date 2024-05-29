1f6ada8cd105dc65600a92662615dd3648e60810
Yixuan Zhu <455484317@qq.com>
2024-05-29 20:44:40 +0800

    weinberg寄了,改用一般方法

--- CMB/old version/CMB.py
+++ CMB/old version/CMB.py
@@ -24,7 +24,7 @@ def solution(log_kC1, O20, H0):
     O10 = 1 - O20
     t0 = 1 / H0
     tspan = (t0, 0)
-    tn = np.linspace(t0, 0, 1000000)
+    tn = np.linspace(t0, 0, 100000)
     # 从t0开始
     zt0 = [0, -H0]
 
@@ -33,19 +33,25 @@ def solution(log_kC1, O20, H0):
     # z.y[0,:] = z(t), z.y[1,:] = z'(t)
     return [z.t, z.y[0, :], z.y[1, :]]
 
+# 方程解的t值到不了0,需要插值
+def interpolation(log_kC1, O20, H0):
+    t, z0, z1 = solution(log_kC1, O20, H0)
+    Z0 = scipy.interpolate.interp1d(t, z0, kind='cubic', fill_value='extrapolate', bounds_error=False)
+    Z1 = scipy.interpolate.interp1d(t, z1, kind='cubic', fill_value='extrapolate', bounds_error=False)
+    return [Z0, Z1]
+
 # 约化尺度因子
 def alpha(log_kC1, O20, H0):
-    z = np.array(solution(log_kC1, O20, H0)[1])
-    return 1 / (1 + z)
+    Z0 = interpolation(log_kC1, O20, H0)[0]
+    a = lambda t: 1 / (1 + Z0(t))
+    return a
 
 # 光子移动距离
-def r_t(log_kC1, O20, H0, z):
-    t_list = np.array(solution(log_kC1, O20, H0)[0])
-    z_list = np.array(solution(log_kC1, O20, H0)[1])
-    idx = np.searchsorted(z_list, z)
-    alpha_list = 1 / alpha(log_kC1, O20, H0)[:idx]
-    r = -np.trapz(alpha_list, t_list[:idx])
-    return r
+def r_t(log_kC1, O20, H0, t):
+    t0 = 1 / H0
+    Z0 = interpolation(log_kC1, O20, H0)[0]
+    r = scipy.integrate.quad(lambda x: (1 + Z0(x)), t, t0)[0]
+    return r * const_c # Mpc
 
 # 计算CMB_TT时需要用到的各类函数,这里采用近似方法
 class CMB_TT:
@@ -60,7 +66,7 @@ class CMB_TT:
         self.tL = others[2]
         self.rL = others[3]
         self.aL = others[4]
-        self.dal = others[5]
+        self.daL = others[5]
         self.RC = others[6]
         self.dD = others[7]
         self.R0 = others[8]
@@ -82,7 +88,7 @@ class CMB_TT:
         return (Delta1 / Delta2) ** 0.25
     # kappa函数
     def kappa(self, q):
-        d_T = 0.035 / ((1 + self.zL) * self.H0) 
+        d_T = 0.035 / ((1 + self.zL) * self.H0) * const_c # Mpc
         return q * d_T / self.aL
     # Rq0参数
     def Rq0(self, q):
@@ -97,39 +103,38 @@ class CMB_TT:
     def dotB(self, q):
         return -2 * self.psi(q) / (q ** 2)
     def ddotB(self, q):
-        # 差分法对psi求导
-        h = 1e-6
+        # 对psi求导
         dpsi1 = 3 * q ** 2 * self.Rq0(q) * self.T(self.kappa(q)) / 5
-        dpsi2 = (self.aL ** 2 - 2 * self.aL * self.dal * self.tL) / self.aL ** 4
+        dpsi2 = (self.aL ** 2 - 2 * self.aL * self.daL * self.tL) / self.aL ** 4
         dpsi = dpsi1 * dpsi2
         return -2 * dpsi / (q ** 2)
     # 朗道阻尼修正
     def Gamma(self, q):
         return q ** 2 * self.dD ** 2 / self.aL ** 2
     # 光子函数中的积分
-    def gamma_intvalue(self, q):
-        t_list = np.array(solution(self.log_kC1, self.O20, self.H0)[0])
-        idx = np.searchsorted(t_list, self.tL)
-        def integrand(q):
-            return q / (alpha(self.log_kC1, self.O20, self.H0)[idx:] * \
-                        np.sqrt(3 * (1 + self.R0 * alpha(self.log_kC1, self.O20, self.H0)[idx:])))
-        return np.trapz(integrand(q), t_list[idx:])
+    def gamma_intvalue(self):
+        Z0 = interpolation(self.log_kC1, self.O20, self.H0)[0]
+        def integrand(t):
+            a = 1 / (1 + Z0(t))
+            return 1 / (a * np.sqrt(3 * (1 + self.R0 * a)))
+        # return scipy.integrate.quad(integrand, 0, self.tL)[0] * const_c # Mpc
+        return 0.1351 * (1 + self.zL) # Mpc
     # 2个光子函数
     def delta_gamma(self, q):
         delta_gamma1 = self.T(self.kappa(q)) * (1 + 3 * self.RL)
         delta_gamma2 = (1 + self.RL) ** -0.25 * np.exp(-self.Gamma(q)) * self.S(self.kappa(q))
-        delta_gamma3 = np.cos(self.gamma_intvalue(q) + self.Delta(self.kappa(q)))
+        delta_gamma3 = np.cos(self.gamma_intvalue() * q + self.Delta(self.kappa(q)))
         return 3 * self.Rq0(q) / 5 * (delta_gamma1 - delta_gamma2 * delta_gamma3)
     def delta_u_gamma(self, q):
         delta_u_gamma1 = self.T(self.kappa(q)) * self.tL
-        delta_u_gamma2 = self.aL / (np.sqrt(3) * (1 + self.R0 * self.aL) ** 0.75) * np.exp(-self.Gamma(q)) * self.S(self.kappa(q))
-        delta_u_gamma3 = np.sin(self.gamma_intvalue(q) + self.Delta(self.kappa(q)))
+        delta_u_gamma2 = self.aL / (np.sqrt(3) * q *(1 + self.RL) ** 0.75) * np.exp(-self.Gamma(q)) * self.S(self.kappa(q))
+        delta_u_gamma3 = np.sin(self.gamma_intvalue() * q + self.Delta(self.kappa(q)))
         return 3 * self.Rq0(q) / 5 * (-delta_u_gamma1 + delta_u_gamma2 * delta_u_gamma3)
     # 2个时间独立函数
     def F(self, q):
         F1 = self.delta_gamma(q) / 3
         F2 = self.aL * self.ddotB(q) / 2
-        F3 = self.aL * self.dal * self.dotB(q) / 2
+        F3 = self.aL * self.daL * self.dotB(q) / 2
         return F1 - F2 - F3
     def G(self, q):
         G1 = self.delta_u_gamma(q) / self.aL
@@ -144,7 +149,7 @@ class CMB_TT:
         def integrand(l, q):
             return (jl(l, q) * self.F(q) + jl_prime(l, q) * self.G(q)) ** 2 * q ** 2
         def intvalue(l):
-            return scipy.integrate.quad(integrand, 0, np.inf, args=(l))[0]
+            return scipy.integrate.quad(integrand, 0, np.inf, limit=100, args=(l))[0]
         return 16 * np.pi ** 2 * self.T0 ** 2 * intvalue(l)
     
 import multiprocessing as mp
@@ -153,32 +158,35 @@ def main():
     log_kC1 = -5
     O20 = 0.28
     H0 = 70 # km/s/Mpc
-    T0 = 2.725 # K
+    T0 = 2.725e6 # uK
     zL = 1090 - 1
-    z_list = np.array(solution(log_kC1, O20, H0)[1])
-    t_list = np.array(solution(log_kC1, O20, H0)[0])
-    idx = np.searchsorted(z_list, zL)
-    tL = t_list[idx] # 1/H0
-    rL = r_t(log_kC1, O20, H0, zL)
-    aL = alpha(log_kC1, O20, H0)[idx]
-    dal = -1 / (1 + zL) ** 2 * np.array(solution(log_kC1, O20, H0)[2])[idx]
+    # 计算tL, rL, aL, daL
+    def L_caculate(log_kC1, O20, H0):
+        Z0 = interpolation(log_kC1, O20, H0)[0]
+        Z1 = interpolation(log_kC1, O20, H0)[1]
+        tL = scipy.optimize.root_scalar(lambda x: Z0(x) - zL, method='newton', x0=0).root
+        rL = r_t(log_kC1, O20, H0, tL)
+        aL = alpha(log_kC1, O20, H0)(tL)
+        daL = -1 / (1 + zL) ** 2 * Z1(tL)
+        return [tL, rL, aL, daL]
+    tL, rL, aL, daL = L_caculate(log_kC1, O20, H0)
     RC = 1.736e-10 * (0.05) ** (1 - 0.95820) # Mpc^(-1)~
     dD = 0.008130 # Mpc
     R0 = 679.6
-    others = [T0, zL, tL, rL, aL, dal, RC, dD, R0]
+    others = [T0, zL, tL, rL, aL, daL, RC, dD, R0]
 
     # 实例化
     CMB = CMB_TT(log_kC1, O20, H0, others)
 
     # 导入数据
-    data = np.loadtxt('CMB/COM_PowerSpect_CMB-TT-full_R3.01.txt', skiprows=30) # l>30
+    data = np.loadtxt('CMB/COM_PowerSpect_CMB-TT-full_R3.01.txt', skiprows=1) # l>30
     l = data[:, 0]
     Dl = data[:, 1]
     dDl_down = data[:, 2]
     dDl_up = data[:, 3]
     
-    # l_list = l
-    l_list = np.linspace(30, 2500, 10)
+    l_list = l
+    # l_list = np.linspace(30, 2500, 50)
     # 计算
     with mp.Pool() as pool:
         C_l = pool.map(CMB.C_l, l_list)
