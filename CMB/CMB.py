1ee1400b771accf0a6c0ec7ecb0952a1d2203f1d
Yixuan Zhu <455484317@qq.com>
2024-06-06 19:32:38 +0800

    maybe we need rewrite camb to python to accelerate the code

--- CMB/CMB.py
+++ CMB/CMB.py
@@ -3,14 +3,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 pars = camb.model.CAMBparams()
-pars.set_cosmology(H0=70, ombh2=0.022, omch2=0.122)
+pars.set_cosmology(H0=67.5, ombh2=0.022, omch2=0.122)
 pars.set_IDM_params(O20=0.28, log_kC1=-2)
-pars.InitPower.set_params(ns=0.965)
+pars.InitPower.set_params(As=2e-9, ns=0.965)
 pars.set_for_lmax(2500)
 
-results = camb.get_results(pars)
-
-powers = results.get_cmb_power_spectra(pars)
+powers = camb.results.CAMBdata().get_cmb_power_spectra(pars)
 
 totCL=powers['total']
 ls = np.arange(totCL.shape[0])
