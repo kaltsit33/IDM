394ab5acd48572bf49e5e302c920dd47c64470f9
Yixuan Zhu <455484317@qq.com>
2024-12-02 15:14:56 +0800

    function has stiff point depend on the value of logkC1

--- README.md
+++ README.md
@@ -3,28 +3,25 @@
 
 #### 2024.11.22更新
 1. 原始论文3篇
->mma用于复现CMB中weinberg给出的近似TT曲线
 2. 展示用代码在[all-in-one.ipynb](all-in-one.ipynb)中
 3. 各方法的数据与参考论文分在不同文件夹中
 4. 由于jupyter无法使用multiprocessing,all-in-one调用不同文件下的单独程序
 5. article and pictures
 
-### multimethods
-1. 利用不同方法的combination进行联合限制
-2. 采取OHD,SNe Ia(Pantheon Plus) & BAO(DESI 2024)
-
-### BAO
-1. 数据来源DESI 2024 & SDSS-IV,[数据文件](/BAO/BAO.csv)
-2. 参考文献[DESI 2024 VI](https://arxiv.org/abs/2404.03002)
-
-### CMB(复活了)
+### CMB与solution
 1. 理论参考书籍为
 >《Cosmology》 S.Weinberg  
 >《Modern Cosmology 2nd》 S.Dodelson & F.Schmidt
 2. 数据来源Planck 2018(PR3) TT,TE,EE & Low-E
 >[camb-python](https://camb.readthedocs.io/en/latest/index.html)  
 >[camb-github](https://github.com/cmbant/CAMB)
-3. CMB计算的rd用于给出BAO限制的h
+3. 方程的是刚性的,刚性的点与$\log kC_1$的取值有关,并在z较大时取到
+>该刚性方程无法用mma与matlab的刚性求解器求解
+4. 由于刚性的存在,$r_{*},r_d$这类需要到0的积分无法给出--BAO与CMB
+
+### BAO
+1. 数据来源DESI 2024 & SDSS-IV,[数据文件](/BAO/BAO.csv)
+2. 参考文献[DESI 2024 VI](https://arxiv.org/abs/2404.03002)
 
 ### OHD
 1. 数据来源见article,[数据文件](/OHD/OHD.csv)
@@ -33,3 +30,9 @@
 ### SNe Ia
 1. Pantheon Plus data from https://github.com/PantheonPlusSH0ES
 2. Pantheon Plus use the $\mu$ (maybe wrong)
+
+### QSO
+1. 验证$\log kC_1$的上限与数据集的z上限有关(方程刚性点)
+
+### multimethods
+1. 利用不同方法的combination进行联合限制
---
+++ test.m
@@ -0,0 +1,45 @@
+% solution.m
+function test(log_kC1, O20, H0)
+    % Unit conversion
+    transfer = 3.24078e-20; % Conversion factor from km/s/Mpc to 1/Gyr
+    kC1 = 10^log_kC1 * transfer;
+    O10 = 1 - O20;
+    t0 = 1 / H0;
+    
+    % Solution interval
+    tspan = [t0, 0.0];
+    
+    % Initial value given at t0
+    zt0 = [0.0, -H0];
+    
+    % Solve the ODE
+    [t, z] = ode15s(@(t, z) function_(t, z, kC1, O10, H0), tspan, zt0);
+    
+    % Plot the results
+    figure;
+    plot(t, z(:, 1), 'DisplayName', 'z(t)');
+    hold on;
+    plot(t, z(:, 2), 'DisplayName', 'z''(t)');
+    xlabel('Time');
+    ylabel('Solution');
+    legend;
+    hold off;
+end
+
+function dz = function_(~, z, kC1, O10, H0)
+    % z(1) = z(t), z(2) = z'(t)
+    dz = zeros(2, 1);
+    dz(1) = z(2);
+    
+    % Reduce the use of parentheses, separate into numerator and denominator
+    numerator = ...
+        H0^4 * kC1 * O10^2 * (z(1)^4 + 1) + ...
+        3 * H0^4 * O10^2 * z(1)^2 * (2 * kC1 - 3 * z(2)) + ...
+        H0^4 * O10^2 * z(1)^3 * (4 * kC1 - 3 * z(2)) - ...
+        3 * H0^4 * O10^2 * z(2) + ...
+        5 * H0^2 * O10 * z(2)^3 - ...
+        kC1 * z(2)^4 + ...
+        H0^2 * O10 * z(1) * (4 * H0^2 * kC1 * O10 - 9 * H0^2 * O10 * z(2) + 5 * z(2)^3);
+    denominator = 2 * H0^2 * O10 * (1 + z(1))^2 * z(2);
+    dz(2) = numerator / denominator;
+end
